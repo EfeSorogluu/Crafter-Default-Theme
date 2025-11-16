170e7008cfc2052c435cc6f5a7ceaac34ea91606
Mert Efe SOROĞLU <sorolu14@gmail.com>
2025-11-16 23:30:25 +0300

    og images fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export async function generateMetadata(): Promise<Metadata> {
       siteName: appConfig.appName,
       images: [
         {
-          url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/og?title=${appConfig.appName}&description=${appConfig.description}&logo=${appConfig.icon192}&brand=${appConfig.appName}`,
+          url: `/api/og?title=${appConfig.appName}&description=${appConfig.description}&logo=${appConfig.icon192}&brand=${appConfig.appName}`,
           width: 1200,
           height: 630,
           alt: appConfig.appName,
