51bd7e2e4e25975c130f80df212e72438004f4f4
Mert Efe SOROĞLU <sorolu14@gmail.com>
2025-12-14 18:48:33 +0300

    [CC-50] Implent new posts v2 endpoint

--- src/lib/services/posts.service.ts
+++ src/lib/services/posts.service.ts
@@ -1,58 +1,74 @@
 import { useApi } from "../hooks/useApi";
 import { BACKEND_URL } from "../constants/base";
 import {
-    WebsitePost,
-    GetPostsParams,
-    PostsResponse,
-    PostLikeResponse,
-    getPostResponse,
+  WebsitePost,
+  GetPostsParams,
+  PostsResponse,
+  PostLikeResponse,
+  getPostResponse,
 } from "../types/posts";
 
 export const useWebsitePostsService = (websiteId?: string) => {
-    const { get, post, delete: del } = useApi({ baseUrl: BACKEND_URL, useWebsiteId: true });
+  const {
+    get,
+    post,
+    delete: del,
+  } = useApi({ baseUrl: BACKEND_URL, useWebsiteId: true, websiteVersion: "v2" });
 
-    const getPosts = async (data: { websiteId: string; params?: GetPostsParams }): Promise<PostsResponse> => {
-        const response = await get<PostsResponse>(
-            `/posts`,
-            { params: data.params },
-            true
-        );
+  const getPosts = async (data: {
+    websiteId: string;
+    params?: GetPostsParams;
+  }): Promise<PostsResponse> => {
+    const response = await get<PostsResponse>(
+      `/posts`,
+      { params: data.params },
+      true
+    );
 
-        return response.data;
-    };
+    return response.data;
+  };
 
-    const getPost = async (data: { websiteId: string; postId: string }): Promise<WebsitePost> => {
-        const response = await get<getPostResponse>(
-            `/posts/${data.postId}`,
-            {},
-            true
-        );
-        return response.data.data;
-    };
+  const getPost = async (data: {
+    websiteId: string;
+    postId: string;
+  }): Promise<WebsitePost> => {
+    const response = await get<getPostResponse>(
+      `/posts/${data.postId}`,
+      {},
+      true
+    );
+    return response.data.data;
+  };
 
-    const likePost = async (data: { websiteId: string; postId: string }): Promise<PostLikeResponse> => {
-        const response = await post<PostLikeResponse>(
-            `/posts/${data.postId}/like`,
-            {},
-            {},
-            true
-        );
-        return response.data;
-    };
+  const likePost = async (data: {
+    websiteId: string;
+    postId: string;
+  }): Promise<PostLikeResponse> => {
+    const response = await post<PostLikeResponse>(
+      `/posts/${data.postId}/like`,
+      {},
+      {},
+      true
+    );
+    return response.data;
+  };
 
-    const unlikePost = async (data: { websiteId: string; postId: string }): Promise<PostLikeResponse> => {
-        const response = await del<PostLikeResponse>(
-            `/posts/${data.postId}/like`,
-            {},
-            true
-        );
-        return response.data;
-    };
+  const unlikePost = async (data: {
+    websiteId: string;
+    postId: string;
+  }): Promise<PostLikeResponse> => {
+    const response = await del<PostLikeResponse>(
+      `/posts/${data.postId}/like`,
+      {},
+      true
+    );
+    return response.data;
+  };
 
-    return {
-        getPosts,
-        getPost,
-        likePost,
-        unlikePost,
-    };
+  return {
+    getPosts,
+    getPost,
+    likePost,
+    unlikePost,
+  };
 };
